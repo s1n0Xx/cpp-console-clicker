2d13a8d4c9a6452c5c623b2c7ae8f09a63d13808
Stepan <smerzlikin20@mail.ru>
2026-03-17 19:10:26 +0500

    feat: добавить механику клика

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@ int main() {
 
     setlocale(LC_ALL, "Russian");
 
-    cout << "Консольный кликер\n";
-    cout << "Скоро здесь будет игра...\n";
+    int score = 0;
+    char input;
+
+    std::cout << "Нажми 'c' чтобы кликать, 'q' чтобы выйти\n";
+
+    while (true) {
+        std::cin >> input;
+
+        if (input == 'c') {
+            score++;
+            std::cout << "Очки: " << score << std::endl;
+        }
+        else if (input == 'q') {
+            break;
+        }
+    }
+
     return 0;
 }

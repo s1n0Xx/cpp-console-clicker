bf824d71c007adc6543e46fb39c44326e9e21012
Stepan <smerzlikin20@mail.ru>
2026-03-17 19:04:20 +0500

    init: создать базовую консольную программу

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Консольный кликер\n";
+    cout << "Скоро здесь будет игра...\n";
+    return 0;
+}
